65d4d402819b3233f03139e878bcb7320b179fb7
JX <jingxian.lau01@gmail.com>
2022-07-09 11:14:50 +0800

    Friend Struct

---
+++ friends/Friend.swift
@@ -0,0 +1,15 @@
+//
+//  Friend.swift
+//  friends
+//
+//  Created by TinkerTanker on 9/7/22.
+//
+
+import Foundation
+struct Friend: Identifiable {
+    
+    var id = UUID()
+    var name: String
+    var details: String
+    
+}
