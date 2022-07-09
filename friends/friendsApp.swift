48a8bbccd0b4afb0083fa60c01a51b27fa54129e
JX <jingxian.lau01@gmail.com>
2022-07-09 11:44:07 +0800

    a lot of stuff

--- friends/friendsApp.swift
+++ friends/friendsApp.swift
@@ -7,11 +7,64 @@
 
 import SwiftUI
 
-@main
-struct friendsApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+struct ContentView: View {
+    @State var friends = [
+        Friends(name: "Felix", details: "a person that plays games"),
+        Friends(name: "Jing Xian", details: "Jing Xian is Jing Xian (math moment)"),
+        Friends(name: "Lindsey", details: "1cm taller than vanisha!"),
+        Friends(name: "En Jie", details: "am late"),
+        Friends(name: "Regina", details: "needs coffee"),
+        Friends(name: "Dishita", details: "can swim")
+    ]
+         
+    var body: some View {
+        NavigationView {
+            VStack {
+                List() {
+                    ForEach(friends) { friend in
+                        NavigationLink {
+//                            FriendDetailView()
+                        } label: {
+                            HStack {
+                                Image(systemName: "trash")
+                                VStack(alignment: .leading) {
+                                    Text(friend.name)
+                                    Text(friend.details)
+                                        .font(.subheadline)
+                                        .opacity(0.8)
+                                }
+                            }
+                        }
+                    }
+                    .onDelete { indexSet in
+                        friends.remove(atOffsets: indexSet)
+                    }
+                    .onMove { indices, newOffset in
+                        friends.move(fromOffsets: indices, toOffset: newOffset)
+                    }
+                }
+                .navigationTitle("Friends")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        EditButton()
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+//                            CreateFriendView()
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+
+                    }
+                }
+            }
         }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
